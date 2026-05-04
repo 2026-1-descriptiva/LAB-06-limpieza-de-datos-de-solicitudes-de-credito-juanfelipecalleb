164ca482ab16767403e912812e2859c7889fd424
juanfelipecalleb <jcalleb@unal.edu.co>
2026-05-03 22:27:40 -0500

    LAB06

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,7 +1,8 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
-
+import pandas as pd
+import os
 
 def pregunta_01():
     """
@@ -13,3 +14,37 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    
+    df = pd.read_csv('files/input/solicitudes_de_credito.csv', sep=';', index_col=0)
+
+    
+    df = df.copy()
+    
+    
+    for column in df.select_dtypes(include=['object']).columns:
+        df[column] = df[column].str.lower()
+        df[column] = df[column].str.replace('_', ' ')
+        df[column] = df[column].str.replace('-', ' ')
+        df[column] = df[column].str.replace(',', '')
+        df[column] = df[column].str.replace('$', '')
+        df[column] = df[column].str.replace('.00', '')
+
+    
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(int)
+
+    
+    df['fecha_de_beneficio'] = pd.to_datetime(df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce").combine_first(pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce"))
+
+    
+    df = df.drop_duplicates()
+
+    
+    df = df.dropna()
+
+    
+    os.makedirs('files/output', exist_ok=True)
+
+    
+    df.to_csv('files/output/solicitudes_de_credito.csv', columns=df.columns, index=False, encoding='utf-8', sep=';')
